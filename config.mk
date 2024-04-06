6cc9e6164221fd1ecd9faa40b15770bc4d78b314
yuval <yuvalaizenman@gmail.com>
2024-04-06 15:50:35 +0300

    Revert "delete this commit"
    This reverts commit 92ee3d98d4bbe9930b1f1cca7fc25c111e0b54ba.

--- config.mk
+++ config.mk
@@ -9,10 +9,8 @@ MANPREFIX = $(PREFIX)/share/man
 
 #X11INC = /usr/X11R6/include
 #X11LIB = /usr/X11R6/lib
-#X11INC = /home/yorks0/libxft/include
-#X11LIB = /home/yorks0/libxft/src/.libs
-X11INC =  /usr/include/X11
-X11LIB =  /usr/include/X11/Xft
+X11INC = /home/yorks0/libxft/include
+X11LIB = /home/yorks0/libxft/src/.libs
 
 PKG_CONFIG = pkg-config
 
